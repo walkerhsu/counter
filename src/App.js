0fcfd599d4b7ec724909b8e99f4e33dd52a07476
walkerhsu <yp10731155@gmail.com>
2022-09-28 14:54:55 +0800

    counter first init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React,{Component} from 'react';
+import Button from './components/buttons';
+import { toHaveDisplayValue } from '@testing-library/jest-dom/dist/matchers';
+class Counter extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count : 100
+    }
+  }
+  render(){
+    return(
+      <div className="App">
+        <h1 className="App-display">
+          {this.state.count}
+        </h1>
+        <span className="App-controls">
+          <Button onClick={this.plus}  text="+" />
+          <Button onClick={this.minus} text="-" />
+        </span>  
+      </div>
+    );
+  }
+  plus = () => {
+    this.setState((state) => ({count: state.count + 1}));
+  }
+  minus = () => {
+    this.setState((state) => ({count: state.count - 1}));
+  }
 }
-
-export default App;
+export default Counter;
---
+++ src/components/buttons.js
@@ -0,0 +1,5 @@
+import React from "react";
+export default ({ onClick, text }) => {
+    return <button onClick={onClick}>{text} 
+    </button>;
+}
